4fc3b117db223d3d816177b35b2d8dd7fb13d892
Owen Henley <owenhenley@me.com>
2023-02-27 10:25:12 +0000

    whack-a-mole

---
+++ whack-a-mole/app.js
@@ -0,0 +1,52 @@
+// Properties
+
+const squares = document.querySelectorAll(".square")
+const mole = document.querySelector(".mole")
+const timeLeft = document.querySelector("#time-left")
+const score = document.querySelector("#score")
+
+let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
+
+let countDownTimerId = setInterval(countDown, 1000)
+
+moveMole()
+
+squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+        // update score
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+function randomSquare() {
+    squares.forEach((square) => {
+        square.classList.remove("mole")
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add("mole")
+
+    hitPosition = randomSquare.id
+}
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 600)
+}
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert(" Your final score is " + result)
+    }
+}
